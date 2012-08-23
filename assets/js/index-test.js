b5f08cc2975de4f75629da9b177327bb699702b9
Trent Ellingsen <trentellingsen@gmail.com>
2012-08-23 13:59:43 -0700

    Took out sample code and added in selection to themes
    booya!

--- assets/js/index-test.js
+++ assets/js/index-test.js
@@ -17,3 +17,16 @@ function setupDebugAutocomplete() {
 		source : debugShortcuts
 	});
 }
+
+function movingBoxes() {
+	$('#slider').movingBoxes({
+		/* width and panelWidth options deprecated, but still work to keep the plugin backwards compatible
+		width: 500,
+		panelWidth: 0.5,
+		*/
+		startPanel   : 1,      // start with this panel
+		wrap         : false,   // if true, the panel will "wrap" (it really rewinds/fast forwards) at the ends
+		buildNav     : true,   // if true, navigation links will be added
+		navFormatter : function(){ return "&#9679;"; } // function which returns the navigation text for each panel
+	});
+}
